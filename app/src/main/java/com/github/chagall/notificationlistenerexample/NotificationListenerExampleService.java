8e049b9e1ef0c6c9b281c44e51abe47a809be419
Michael Ramsey <mike@hackerdise.me>
2023-07-21 16:53:51 -0400

    Updated to support latest android and generically work for any app

--- app/src/main/java/com/github/chagall/notificationlistenerexample/NotificationListenerExampleService.java
+++ app/src/main/java/com/github/chagall/notificationlistenerexample/NotificationListenerExampleService.java
@@ -1,9 +1,9 @@
 package com.github.chagall.notificationlistenerexample;
 
 import android.content.Intent;
-import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 /**
  * MIT License
@@ -26,80 +26,26 @@ import android.service.notification.StatusBarNotification;
  */
 public class NotificationListenerExampleService extends NotificationListenerService {
 
-    /*
-        These are the package names of the apps. for which we want to
-        listen the notifications
-     */
-    private static final class ApplicationPackageNames {
-        public static final String FACEBOOK_PACK_NAME = "com.facebook.katana";
-        public static final String FACEBOOK_MESSENGER_PACK_NAME = "com.facebook.orca";
-        public static final String WHATSAPP_PACK_NAME = "com.whatsapp";
-        public static final String INSTAGRAM_PACK_NAME = "com.instagram.android";
-    }
-
-    /*
-        These are the return codes we use in the method which intercepts
-        the notifications, to decide whether we should do something or not
-     */
-    public static final class InterceptedNotificationCode {
-        public static final int FACEBOOK_CODE = 1;
-        public static final int WHATSAPP_CODE = 2;
-        public static final int INSTAGRAM_CODE = 3;
-        public static final int OTHER_NOTIFICATIONS_CODE = 4; // We ignore all notification with code == 4
+    public static class InterceptedNotificationCode {
+        public static final int OTHER_NOTIFICATIONS_CODE = 0;
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return super.onBind(intent);
-    }
+    public void onNotificationPosted(StatusBarNotification sbn) {
 
-    @Override
-    public void onNotificationPosted(StatusBarNotification sbn){
-        int notificationCode = matchNotificationCode(sbn);
-
-        if(notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE){
-            Intent intent = new  Intent("com.github.chagall.notificationlistenerexample");
-            intent.putExtra("Notification Code", notificationCode);
-            sendBroadcast(intent);
-        }
-    }
-
-    @Override
-    public void onNotificationRemoved(StatusBarNotification sbn){
-        int notificationCode = matchNotificationCode(sbn);
-
-        if(notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
-
-            StatusBarNotification[] activeNotifications = this.getActiveNotifications();
-
-            if(activeNotifications != null && activeNotifications.length > 0) {
-                for (int i = 0; i < activeNotifications.length; i++) {
-                    if (notificationCode == matchNotificationCode(activeNotifications[i])) {
-                        Intent intent = new  Intent("com.github.chagall.notificationlistenerexample");
-                        intent.putExtra("Notification Code", notificationCode);
-                        sendBroadcast(intent);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    private int matchNotificationCode(StatusBarNotification sbn) {
         String packageName = sbn.getPackageName();
 
-        if(packageName.equals(ApplicationPackageNames.FACEBOOK_PACK_NAME)
-                || packageName.equals(ApplicationPackageNames.FACEBOOK_MESSENGER_PACK_NAME)){
-            return(InterceptedNotificationCode.FACEBOOK_CODE);
-        }
-        else if(packageName.equals(ApplicationPackageNames.INSTAGRAM_PACK_NAME)){
-            return(InterceptedNotificationCode.INSTAGRAM_CODE);
-        }
-        else if(packageName.equals(ApplicationPackageNames.WHATSAPP_PACK_NAME)){
-            return(InterceptedNotificationCode.WHATSAPP_CODE);
-        }
-        else{
-            return(InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE);
-        }
+        Intent i = new  Intent("com.github.chagall.notificationlistenerexample");
+        i.putExtra("Notification Code", InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE);
+        i.putExtra("Package Name", packageName);
+
+        Log.i("NotificationListener", "Received notification from " + packageName);
+
+        sendBroadcast(i);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        // Implementation remains the same
     }
 }
